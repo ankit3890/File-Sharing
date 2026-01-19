3a24fae2887bb39384317aadf7f6232ffa446457
ankit3890 <ankitidea3890@gmail.com>
2026-01-20 01:26:37 +0530

    Add server-side request logging

--- server/index.js
+++ server/index.js
@@ -13,6 +13,10 @@ const app = express();
 connectDB();
 
 // Middleware
+app.use((req, res, next) => {
+    console.log(`[REQUEST] ${req.method} ${req.url}`);
+    next();
+});
 app.use(express.json());
 app.use(cors());
 app.use(helmet({
